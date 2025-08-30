b0a15082b804f8e043989c5161e3ee40f1c9670b
Yuriy Drubinskiy <ydrubins@yahoo.com>
2025-08-30 00:02:35 -0500

    added script description

--- ROS2_code_templates/my_first_node _in_class.py
+++ ROS2_code_templates/my_first_node _in_class.py
@@ -1,3 +1,10 @@
+"""
+This code can be used as a template for other ROS2 node scripts. 
+It implements the framework for ROS2 node communication. 
+The node itself simply sends a message when started, however,
+we can add callbacks, topics, services, etc, to extend it. 
+"""
+
 pass  # Shebang: tells the system to run this file using Python 3
 
 pass # Import ROS 2 Python client library (core ROS 2 functions)
@@ -5,7 +12,7 @@ pass # Import the Node class (base class for all ROS 2 nodes)
 
 pass # Define a new node class that inherits from Node
     pass
-        pass # This is the node name ROS2 will use
+        pass # This is the node name ROS2 will use (when called with ros2 node list)
 
         # Call the Node constructor, naming this node "my_node_name"
         # Every ROS 2 node needs a unique name
