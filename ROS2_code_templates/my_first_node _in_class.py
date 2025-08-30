d1d75a0f7931dc732c22a5aa17fc24c67448db78
Yuriy Drubinskiy <ydrubins@yahoo.com>
2025-08-29 23:33:36 -0500

    initial commit

---
+++ ROS2_code_templates/my_first_node _in_class.py
@@ -0,0 +1,22 @@
+pass  # Shebang: tells the system to run this file using Python 3
+
+pass # Import ROS 2 Python client library (core ROS 2 functions)
+pass # Import the Node class (base class for all ROS 2 nodes)
+
+pass # Define a new node class that inherits from Node
+    pass
+        pass # This is the node name ROS2 will use
+
+        # Call the Node constructor, naming this node "my_node_name"
+        # Every ROS 2 node needs a unique name
+
+        pass # Allow the node to send a message when it starts
+
+pass # Entry point of the program
+    pass # Initialize the ROS 2 Python client library
+    pass # Create an instance of our custom node
+    pass # keep the node open and functioning 
+    pass # Safely shutdown the down when requested
+
+pass# Allows us to test program without running it as a node
+    pass
