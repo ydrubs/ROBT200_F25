4ae0017ac9a1702671ca13b8395110d882432e0b
Yuriy Drubinskiy <ydrubins@yahoo.com>
2025-11-11 10:20:30 -0600

    code update FULL

--- ROS2_code_templates/number_counter.py
+++ ROS2_code_templates/number_counter.py
@@ -1,9 +1,9 @@
-
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Int64
 from my_robot_interfaces.srv import ResetCounter
+
 
 class NumberCounterNode(Node):
     def __init__(self):
@@ -17,20 +17,20 @@ class NumberCounterNode(Node):
             - callback function (self.callback_number)
             - Buffer depth size (10)
         """
-        self.number_subscriber = self. create_subscription(
-                                Int64, 
-                                "number", 
-                                self.callback_number, 
-                                10)
-        
+        self.number_subscriber = self.create_subscription(
+            Int64,
+            "number",
+            self.callback_number,
+            10)
+
         """
         Create a service server. We need to specify the following parameters:
             - Interface we are using for the service. This needs to be the same for the server and client (ResetCounter)
             - The name of the service that the server and client will be using (similar to a topic name)
             - THe callback to the function that triggers when a client connects
         """
-        self.reset_counter = self.create_service(ResetCounter, 
-                                                 "reset_counter", 
+        self.reset_counter = self.create_service(ResetCounter,
+                                                 "reset_counter",
                                                  self.callback_reset_counter)
 
         self.get_logger().info("Hello world")
@@ -40,46 +40,46 @@ class NumberCounterNode(Node):
     In the function definition, a parameter (msg) will store incoming data (it can technically be called anything else, but msg is a logical name)
     It is also good practice to specifiy the interface type (Int64), not required 
     """
-    def callback_number(self, msg : Int64):
-        self.counter += msg.data # Adds the value that the publisher sends to a counter (since our publisher is sending a number)
-        self.get_logger().info(f"Counter value is at: {self.counter}") # Display the value of the counter in the terminal
 
+    def callback_number(self, msg: Int64):
+        self.counter += msg.data  # Adds the value that the publisher sends to a counter (since our publisher is sending a number)
+        self.get_logger().info(
+            f"Counter value is at: {self.counter}")  # Display the value of the counter in the terminal
 
-    def callback_reset_counter_min(self, request : ResetCounter.Request, responce : ResetCounter.Response):
+    def callback_reset_counter_min(self, request: ResetCounter.Request, responce: ResetCounter.Response):
 
-        self.counter = request.reset_value # Set the counter equal to the reset value requested by the client
-        self.get_logger().info(f"Reset counter to {self.counter}") # Debug info from the node 
-        responce.success = True # Format a return messgae to the client holding a boolean that the counter was reset 
-        responce.message = "Succuess" # Send back a responce message
-        return responce # Send the message to the client
+        self.counter = request.reset_value  # Set the counter equal to the reset value requested by the client
+        self.get_logger().info(f"Reset counter to {self.counter}")  # Debug info from the node
+        responce.success = True  # Format a return messgae to the client holding a boolean that the counter was reset
+        responce.message = "Succuess"  # Send back a responce message
+        return responce  # Send the message to the client
 
+    def callback_reset_counter(self, request: ResetCounter.Request, responce: ResetCounter.Response):
 
-    def callback_reset_counter(self, request : ResetCounter.Request, responce : ResetCounter.Response):
-        
         # since ROS knows that 'request' is an interface from the server (since it's in the contructor), we can check it against the reset_value field of our interfaec (ResetCounter)
-        if request.reset_value < 0: # If a negative value is requested
-            responce.success = False # Return False, do not reset counter
-            responce.message = "Cannot reset counter to a negative value" # Structure a responce string letting client know the issue
-        
-        elif request.reset_value > self.counter: # if a value greater then the current count is requested 
+        if request.reset_value < 0:  # If a negative value is requested
+            responce.success = False  # Return False, do not reset counter
+            responce.message = "Cannot reset counter to a negative value"  # Structure a responce string letting client know the issue
+
+        elif request.reset_value > self.counter:  # if a value greater then the current count is requested
             responce.success = False
             responce.message = "Reset value has to be lower then current value"
-        
-        else: # Otherwise fulfill the request
-            self.counter = request.reset_value
-            self.get_logger().info(f"Reset counter to {self.counter}") # Debug info from the node 
-            responce.success = True # Format a return messgae to the client holding a boolean that the counter was reset 
-            responce.message = "Succuess" # Send back a responce message
 
-        return responce # Send back the responce with the 'success' and 'message' fields filled out
+        else:  # Otherwise fulfill the request
+            self.counter = request.reset_value
+            self.get_logger().info(f"Reset counter to {self.counter}")  # Debug info from the node
+            responce.success = True  # Format a return messgae to the client holding a boolean that the counter was reset
+            responce.message = "Succuess"  # Send back a responce message
+
+        return responce  # Send back the responce with the 'success' and 'message' fields filled out
 
 
 def main(args=None):
+    rclpy.init(args=args)  # Initialize the ROS2 Python client library
+    node = NumberCounterNode()  # Create an instance of the custom node (registers it with ROS2)
+    rclpy.spin(node)  # Keep the node running and responsive to callbacks (e.g., subscriptions, timers)
+    rclpy.shutdown()  # Cleanly shut down the ROS2 client library after the node is done
 
-    rclpy.init(args=args)            # Initialize the ROS2 Python client library
-    node = NumberCounterNode()            # Create an instance of the custom node (registers it with ROS2)
-    rclpy.spin(node)                 # Keep the node running and responsive to callbacks (e.g., subscriptions, timers)
-    rclpy.shutdown()                 # Cleanly shut down the ROS2 client library after the node is done
 
 if __name__ == '__main__':
     main()
--- ROS2_code_templates/number_publisher.py
+++ ROS2_code_templates/number_publisher.py
@@ -2,15 +2,33 @@
 import rclpy
 from rclpy.node import Node
 
-from example_interfaces.msg import Int64 # Import the interface 
+from example_interfaces.msg import Int64  # Import the interface
+
+from rclpy.parameter import Parameter  # Import to allow for parameter callback
+
 
 class NumberPublisherNode(Node):
     def __init__(self):
-        super().__init__("number_publisher") # name of the node
+        super().__init__("number_publisher_w_params")  # name of the node
 
-        #Add an attribute to keep count
-        self.number = 2
-        
+        # Below we create parameters for the starting number and the update frequency
+        # We then need to update out publisher with the associated parameters
+        self.declare_parameter("number", 2)
+        self.declare_parameter("publish_period", 1.0)
+
+        # Register a parameter callback
+        # Name of callback is the only argument needed
+        # This allows parameter to be changed during run-time
+        self.add_post_set_parameters_callback(self.parameters_callback)
+
+        # Add an attribute to keep count
+        # self.number = 2
+
+        # Instead of setting a static number like in the line above, we can now grab the value of the parameter.
+        # We do the same for the frequency
+        self.number = self.get_parameter("number").value
+        self.timer_period = self.get_parameter("publish_period").value
+
         """
         Create the number publisher with the following parameters:
             Topic interface: We’ll use Int64 from the example_interfaces package.
@@ -20,24 +38,28 @@ class NumberPublisherNode(Node):
         self.number_publisher = self.create_publisher(Int64, "number", 10)
 
         # Add an an attribute to trigger a callback based on a timer
-        self.number_timer = self.create_timer(1.0, self.publish_number)
+        self.number_timer = self.create_timer(self.timer_period, self.publish_number)
 
         # Add a log to show some feedback when the node starts
         self.get_logger().info("Number Publisher Started.")
 
     def publish_number(self):
-        msg = Int64() # This is the variable that holds the interface that will be sent
-        msg.data = self.number # The interface is stored as ‘data’ and its value is the value of our self.number attribute. We know the field is called ‘data’ because if we put in ros2 interface show example_interfaces/msg/Int64 in the CLI we see that field. 
-        self.number_publisher.publish(msg) # This command is what actually publishes the message 
+        msg = Int64()  # This is the variable that holds the interface that will be sent
+        msg.data = self.number  # The interface is stored as ‘data’ and its value is the value of our self.number attribute. We know the field is called ‘data’ because if we put in ros2 interface show example_interfaces/msg/Int64 in the CLI we see that field.
+        self.number_publisher.publish(msg)  # This command is what actually publishes the message
+
+    def parameters_callback(self, params: list[Parameter]):
+        for param in params:
+            if param.name == 'number':
+                self.number = param.value
+
 
 def main(args=None):
-    rclpy.init(args=args) # initialize the ROS2 client library
-    node = NumberPublisherNode() # Create an object from the class
-    rclpy.spin(node) # Keep the node running
-    rclpy.shutdown() # Allows node to be shutdown with ctrl + C or other methods
+    rclpy.init(args=args)  # initialize the ROS2 client library
+    node = NumberPublisherNode()  # Create an object from the class
+    rclpy.spin(node)  # Keep the node running
+    rclpy.shutdown()  # Allows node to be shutdown with ctrl + C or other methods
 
-
- 
 
 if __name__ == "__main__":
     main()
